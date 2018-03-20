e42e2b470d5ea80f3d68d165113b2a56a587358e
antoinelanco <antoinelanco@gmail.com>
2018-03-20 16:14:27 +0100

    .h

--- src/Labyrinthe.h
+++ src/Labyrinthe.h
@@ -3,14 +3,21 @@
 
 #include "Environnement.h"
 
+
 class Labyrinthe : public Environnement {
 private:
+	int lab_w;
+	int lab_h;
+	char**	_data;
 
 public:
 	Labyrinthe (char*);
-	int width ();	// retourne la largeur du labyrinthe.
-	int height ();	// retourne la longueur du labyrinthe.
-	char data (int i, int j);
+	int width () { return lab_w;}	// retourne la largeur du labyrinthe.
+	int height () { return lab_h;}	// retourne la longueur du labyrinthe.
+	char data (int i, int j)
+	{
+		return _data [i][j];
+	}	// retourne la case (i, j).
 };
 
 #endif
