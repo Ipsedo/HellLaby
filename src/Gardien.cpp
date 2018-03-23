482848d7f9eb7f054e9a929baac288baaa121b9f
samuel <berriens@gmail.com>
2018-03-23 11:04:17 +0100

    fait mouvement rebondissant sur mur

--- src/Gardien.cpp
+++ src/Gardien.cpp
@@ -3,27 +3,45 @@
 #include <cstdlib>
 #include <ctime>
 #include <iostream>
+#include <math.h>
 
 Gardien::Gardien (Labyrinthe* l, const char* modele) : Mover (120, 80, l, modele) {
 	std::srand(std::time(nullptr));
 }
 
 void Gardien::update (void) {
-	double dx = (double) 0.5 * std::rand() / RAND_MAX - 0.25;
-	double dy = (double) 0.5 * std::rand() / RAND_MAX - 0.25;
-	this->move(dx, dy);
+	// double dx = (double) 2. * std::rand() / RAND_MAX - 1.;
+	// double dy = (double) 2. * std::rand() / RAND_MAX - 1.;
+	// _angle++;
+	double a = 2. * M_PI * _angle / 360.;
+	double dx = cos(a);
+	double dy = sin(a);
+
+	float x = _x + dx;
+	float y = _y + dy;
+	int oldX = _x / _l->scale;
+	int oldY = _y / _l->scale;
+	int posX = (int) x / _l->scale;
+	int posY = (int) y / _l->scale;
+	if ((oldX != posX || oldY != posY) && _l->data(posX, posY) == FULL)
+		_angle = 360. * (double) std::rand() / RAND_MAX;
+	else
+		this->move(dx, dy);
 }
 
 bool Gardien::move (double dx, double dy) {
-	double x = _x + dx * (double) _l->scale;
-	double y = _y + dy * (double) _l->scale;
-	int posX = x / _l->scale;
-	int posY = y / _l->scale;
-	std::cout << posX << " " << posY << std::endl;
-	if (_l->data(posX, posY) == 1)
+	/*float x = _x + dx;
+	float y = _y + dy;
+	int oldX = _x / _l->scale;
+	int oldY = _y / _l->scale;
+	int posX = (int) x / _l->scale;
+	int posY = (int) y / _l->scale;
+	if ((oldX != posX || oldY != posY) && _l->data(posX, posY) == FULL)
 		return false;
 	_x = x;
-	_y = y;
+	_y = y;*/
+	_x += dx;
+	_y += dy;
 	return true;
 }
 
